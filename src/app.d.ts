2a6514ab4f224e194d1952fc98e1e63871636464
Noel Rivas <hola@noelr.dev>
2026-06-03 10:58:18 -0600

    Import path fix in definitions file.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,20 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-import type { auth } from '$lib/server/auth';
+import type { auth } from '$lib/auth';
 
 type AuthSession = Awaited<ReturnType<typeof auth.api.getSession>>;
 
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			user: NonNullable<AuthSession>['user'] | null;
-			session: NonNullable<AuthSession>['session'] | null;
-		}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    interface Locals {
+      user: NonNullable<AuthSession>['user'] | null;
+      session: NonNullable<AuthSession>['session'] | null;
+    }
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
-export {};
+export { };
