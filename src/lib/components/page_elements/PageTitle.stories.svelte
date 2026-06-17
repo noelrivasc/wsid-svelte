6fd10eba21258d0a7f3eacf723b99e57d63da30c
Noel Rivas <hola@noelr.dev>
2026-06-16 19:02:56 -0600

    Add PageTitle element

---
+++ src/lib/components/page_elements/PageTitle.stories.svelte
@@ -0,0 +1,18 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import PageTitle from '$lib/components/page_elements/PageTitle.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/PageElements/PageTitle',
+    component: PageTitle,
+    tags: ['autodocs'],
+    argTypes: {
+      title: { control: 'text' }
+    },
+    args: {
+      title: 'Sample title'
+    }
+  });
+</script>
+
+<Story name="Sample" />
---
+++ src/lib/components/page_elements/PageTitle.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  type Props = {
+    title: string;
+  };
+  let { title }: Props = $props();
+</script>
+
+<h1 class="text-xl text-body md:text-2xl">{title}</h1>
